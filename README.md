4cad46dc664e0f3af35181cd95826bd16ca1dfdd
vvasu001 <vvasu001@gmail.com>
2018-09-25 20:03:48 +0530

    Update Config & README.md

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Sharepost APP
 
 A small PHP OOP App Blog which lets you have CRUD functionality.
+
+## Steps
+
+1. Go to app/config->config.php
+2. Put your DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, URLROOT.
+3. Put the directory into htdocs folder of XAMPP, WAMP, MAMP, etc.
+4. Go to public/db and import the db file to phpmyadmin directly. No need to create new database.
+4. Go to http://localhost/shareposts/ to run the app.
+
+## Note
+
+You will have to edit .htaccess file in public folder if you're deploying it anywhere else. Currently, Rewrite is /shareposts/public which means it is accessing shareposts/public folder of htdocs. Change it according to your server.
---
+++ public/db/shareposts.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 25, 2018 at 04:30 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shareposts`
+--
+CREATE DATABASE IF NOT EXISTS `shareposts` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `shareposts`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` mediumtext NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
